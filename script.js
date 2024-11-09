2a4fecf409c7fe6e8860f70d379ce20eec3d8533
Diego-art-code <dasr31052000@gmail.com>
2024-11-09 15:22:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-// Función para mostrar el contador de tiempo de la oferta especial
-function startCountdown() {
-    const countdownElement = document.getElementById("countdown");
-    const targetDate = new Date().getTime() + (24 * 60 * 60 * 1000); // 1 día de descuento
-
-    const interval = setInterval(function() {
-        const now = new Date().getTime();
-        const timeRemaining = targetDate - now;
-
-        if (timeRemaining <= 0) {
-            clearInterval(interval);
-            countdownElement.innerHTML = "Oferta Expirada";
-        } else {
-            const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
-            const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
-            countdownElement.innerHTML = hours + "h " + minutes + "m " + seconds + "s ";
-        }
-    }, 1000);
-}
 // Obtener el carrito desde el almacenamiento local o iniciar un carrito vacío
 let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
 
@@ -61,6 +41,27 @@ buttons.forEach(button => {
         agregarAlCarrito(name, price);
     });
 });
+// Función para mostrar el contador de tiempo de la oferta especial
+function startCountdown() {
+    const countdownElement = document.getElementById("countdown");
+    const targetDate = new Date().getTime() + (24 * 60 * 60 * 1000); // 1 día de descuento
+
+    const interval = setInterval(function() {
+        const now = new Date().getTime();
+        const timeRemaining = targetDate - now;
+
+        if (timeRemaining <= 0) {
+            clearInterval(interval);
+            countdownElement.innerHTML = "Oferta Expirada";
+        } else {
+            const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+            const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+            countdownElement.innerHTML = hours + "h " + minutes + "m " + seconds + "s ";
+        }
+    }, 1000);
+}
+
 
 // Llamar a la función para actualizar el carrito cuando se cargue la página
 actualizarCarrito();
